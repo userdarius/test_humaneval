5d0c17f1d770900ac1474f7c2b28284d5101e619
Darius Foodeei <darius.foodei@gmail.com>
2024-12-28 14:48:39 +0100

    removed p_ik and p_true methods

--- scores.py
+++ scores.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 def context_entails_response(context, responses, model):
     votes = []
     for response in responses:
         votes.append(model.check_implication(context, response))
     return 2 - np.mean(votes)
+
 
 def get_semantic_ids(strings_list, model, strict_entailment=False, example=None):
     """Group list of predictions into semantic meaning."""
@@ -12,7 +14,9 @@ def get_semantic_ids(strings_list, model, strict_entailment=False, example=None)
     def are_equivalent(text1, text2):
 
         implication_1 = model.check_implication(text1, text2, example=example)
-        implication_2 = model.check_implication(text2, text1, example=example)  # pylint: disable=arguments-out-of-order
+        implication_2 = model.check_implication(
+            text2, text1, example=example
+        )  # pylint: disable=arguments-out-of-order
         assert (implication_1 in [0, 1, 2]) and (implication_2 in [0, 1, 2])
 
         if strict_entailment:
@@ -21,7 +25,9 @@ def get_semantic_ids(strings_list, model, strict_entailment=False, example=None)
         else:
             implications = [implication_1, implication_2]
             # Check if none of the implications are 0 (contradiction) and not both of them are neutral.
-            semantically_equivalent = (0 not in implications) and ([1, 1] != implications)
+            semantically_equivalent = (0 not in implications) and (
+                [1, 1] != implications
+            )
 
         return semantically_equivalent
 
@@ -34,7 +40,7 @@ def get_semantic_ids(strings_list, model, strict_entailment=False, example=None)
         if semantic_set_ids[i] == -1:
             # If string1 has not been assigned an id, assign it next_id.
             semantic_set_ids[i] = next_id
-            for j in range(i+1, len(strings_list)):
+            for j in range(i + 1, len(strings_list)):
                 # Search through all remaining strings. If they are equivalent to string1, assign them the same id.
                 if are_equivalent(string1, strings_list[j]):
                     semantic_set_ids[j] = next_id
@@ -45,7 +51,7 @@ def get_semantic_ids(strings_list, model, strict_entailment=False, example=None)
     return semantic_set_ids
 
 
-def logsumexp_by_id(semantic_ids, log_likelihoods, agg='sum_normalized'):
+def logsumexp_by_id(semantic_ids, log_likelihoods, agg="sum_normalized"):
     """Sum probabilities with the same semantic id.
 
     Log-Sum-Exp because input and output probabilities in log space.
@@ -59,7 +65,7 @@ def logsumexp_by_id(semantic_ids, log_likelihoods, agg='sum_normalized'):
         id_indices = [pos for pos, x in enumerate(semantic_ids) if x == uid]
         # Gather log likelihoods at these indices.
         id_log_likelihoods = [log_likelihoods[i] for i in id_indices]
-        if agg == 'sum_normalized':
+        if agg == "sum_normalized":
             # log_lik_norm = id_log_likelihoods - np.prod(log_likelihoods)
             log_lik_norm = id_log_likelihoods - np.log(np.sum(np.exp(log_likelihoods)))
             logsumexp_value = np.log(np.sum(np.exp(log_lik_norm)))
@@ -104,175 +110,8 @@ def cluster_assignment_entropy(semantic_ids):
 
     n_generations = len(semantic_ids)
     counts = np.bincount(semantic_ids)
-    probabilities = counts/n_generations
+    probabilities = counts / n_generations
     assert np.isclose(probabilities.sum(), 1)
-    entropy = - (probabilities * np.log(probabilities)).sum()
+    entropy = -(probabilities * np.log(probabilities)).sum()
     return entropy
-
-
-### P_true ###
-"""Compute p_true uncertainty metric."""
-import logging
-
-
-def construct_few_shot_prompt(
-        *, model, dataset, indices, prompt, brief, brief_always, make_prompt,
-        num_generations, metric):
-    """Construct few shot prompt for p_true uncertainty metric."""
-
-    # Call model n_shots many times.
-    few_shot_prompt = []
-    all_responses = dict()
-    for it, i in enumerate(indices):
-        prompt_candidate = []
-        example = dataset[i]
-        question = example["question"]
-        context = example["context"]
-        if it != 0:
-            prompt_candidate += ['\n']
-        prompt_candidate += ['Question: ' + question]
-        prompt_candidate += ['\nBrainstormed Answers: ']
-        current_question = make_prompt(context, question, None, brief, brief_always)
-        local_prompt = prompt + current_question
-        logging.info('P_TRUE >> Current Question: '.ljust(25) + current_question)
-
-        responses = []
-        for j in range(num_generations + 1):
-
-            if j == 0:
-                temperature = 0.1
-            else:
-                temperature = 1.0
-
-            response, _, _ = model.predict(local_prompt, temperature)
-            logging.info('P_TRUE >> Current Response: '.ljust(25) + response)
-
-            responses.append(response)
-            prompt_candidate += [f'{response.strip()} \n']
-            if j == 0:
-                # Save most likely response and compute correctness metric for it.
-                most_likely_response = response
-                is_correct = metric(response, example, model)
-                answers = [answer for answer in example['answers']['text']]
-                logging.info('P_TRUE >> LOW-T >> true answer: '.ljust(35) + str(answers))
-                logging.info('P_TRUE >> LOW-T >> acc: '.ljust(35) + str(is_correct))
-
-        all_responses[i] = dict(
-            responses=responses, most_likely_response=most_likely_response,
-            is_correct=is_correct)
-
-        prompt_candidate += ['Possible answer: ' + most_likely_response + '\n']
-        prompt_candidate += ['Is the possible answer:\n']
-        prompt_candidate += ['A) True\n']
-        prompt_candidate += ['B) False\n']
-        prompt_candidate += ['The possible answer is:']
-        prompt_candidate += [' A' if is_correct else ' B']
-
-        prompt_len = len(model.tokenizer.encode(''.join(few_shot_prompt + prompt_candidate)))
-        # At test time, get a maximum of `num_generations * model.token_limit` extra tokens
-        # 200 buffer for question and 'Possible Answer'.
-        max_input_len = prompt_len + num_generations * model.max_new_tokens + 200
-
-        if max_input_len < model.token_limit:
-            few_shot_prompt.extend(prompt_candidate)
-        else:
-            logging.warning('Cutting of p_true prompt at length %d.', it)
-            break
-
-    return ''.join(few_shot_prompt), all_responses, it
-
-
-def calculate_p_true(
-        model, question, most_probable_answer, brainstormed_answers,
-        few_shot_prompt, hint=False):
-    """Calculate p_true uncertainty metric."""
-
-    if few_shot_prompt:
-        prompt = few_shot_prompt + '\n'
-    else:
-        prompt = ''
-
-    prompt += 'Question: ' + question
-    prompt += '\nBrainstormed Answers: '
-    for answer in brainstormed_answers + [most_probable_answer]:
-        prompt += answer.strip() + '\n'
-    prompt += 'Possible answer: ' + most_probable_answer + '\n'
-    if not hint:
-        prompt += 'Is the possible answer:\n'
-        prompt += 'A) True\n'
-        prompt += 'B) False\n'
-        prompt += 'The possible answer is:'
-    else:
-        prompt += 'Do the brainstormed answers match the possible answer? Respond with A if they do, if they do not respond with B. Answer:'
-
-    log_prob = model.get_p_true(prompt)
-
-    return log_prob
-
-
-"""Predict model correctness from linear classifier."""
-import logging
-import torch
-import wandb
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
-
-
-def get_p_ik(train_embeddings, is_false, eval_embeddings=None, eval_is_false=None):
-    """Fit linear classifier to embeddings to predict model correctness."""
-
-    logging.info('Accuracy of model on Task: %f.', 1 - torch.tensor(is_false).mean())  # pylint: disable=no-member
-
-    # Convert the list of tensors to a 2D tensor.
-    train_embeddings_tensor = torch.cat(train_embeddings, dim=0)  # pylint: disable=no-member
-    # Convert the tensor to a numpy array.
-    embeddings_array = train_embeddings_tensor.cpu().numpy()
-
-    # Split the data into training and test sets.
-    X_train, X_test, y_train, y_test = train_test_split(  # pylint: disable=invalid-name
-        embeddings_array, is_false, test_size=0.2, random_state=42)  # pylint: disable=invalid-name
-
-    # Fit a logistic regression model.
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-
-    # Predict deterministically and probabilistically and compute accuracy and auroc for all splits.
-    X_eval = torch.cat(eval_embeddings, dim=0).cpu().numpy()  # pylint: disable=no-member,invalid-name
-    y_eval = eval_is_false
-
-    Xs = [X_train, X_test, X_eval]  # pylint: disable=invalid-name
-    ys = [y_train, y_test, y_eval]  # pylint: disable=invalid-name
-    suffixes = ['train_train', 'train_test', 'eval']
-
-    metrics, y_preds_proba = {}, {}
-
-    for suffix, X, y_true in zip(suffixes, Xs, ys):  # pylint: disable=invalid-name
-
-        # If suffix is eval, we fit a new model on the entire training data set
-        # rather than just a split of the training data set.
-        if suffix == 'eval':
-            model = LogisticRegression()
-            model.fit(embeddings_array, is_false)
-            convergence = {
-                'n_iter': model.n_iter_[0],
-                'converged': (model.n_iter_ < model.max_iter)[0]}
-
-        y_pred = model.predict(X)
-        y_pred_proba = model.predict_proba(X)
-        y_preds_proba[suffix] = y_pred_proba
-        acc_p_ik_train = accuracy_score(y_true, y_pred)
-        auroc_p_ik_train = roc_auc_score(y_true, y_pred_proba[:, 1])
-        split_metrics = {
-            f'acc_p_ik_{suffix}': acc_p_ik_train,
-            f'auroc_p_ik_{suffix}': auroc_p_ik_train}
-        metrics.update(split_metrics)
-
-    logging.info('Metrics for p_ik classifier: %s.', metrics)
-    wandb.log({**metrics, **convergence})
-
-    # Return model predictions on the eval set.
-    return y_preds_proba['eval'][:, 1]
 
